97dae34103d2d2a1b6f718bbbb27c430b9fa1580
Sam Moore <swmoore84@gmail.com>
2021-05-12 23:40:03 +0900

    add todo controller action

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,4 +1,10 @@
 class TodosController < ApplicationController
   def index
   end
+
+  def all_todos
+    completed = Todo.where(completed: true)
+    uncompleted = Todo.where(completed: false)
+    render json: { completed: completed, uncompleted: uncompleted }
+  end
 end
