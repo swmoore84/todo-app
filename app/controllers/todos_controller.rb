338ca144ab7adb8fa94e0c403d8d9309a6458c5d
Sam Moore <swmoore84@gmail.com>
2021-05-17 23:16:21 +0900

    update todo controller

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -4,7 +4,22 @@ class TodosController < ApplicationController
 
   def all_todos
     completed = Todo.where(completed: true)
-    uncompleted = Todo.where(completed: false)
-    render json: { completed: completed, uncompleted: uncompleted }
+    pending = Todo.where(completed: false).order(:id)
+    render json: { completed: completed, pending: pending }
+  end
+
+  def update
+    todo = Todo.find(params[:id])
+    if todo.update_attributes!(todo_params)
+      render json: { message: "Todo Item updated successfully" }
+    else
+      render json: { message: "An error occured" }
+    end
+  end
+
+  private
+
+  def todo_params
+    params.require(:todo).permit(:id, :title, :completed)
   end
 end
